88c9ef820886b29f02bbd9a84f574b554801131a
Adam Boudreau <adamdboudreau@gmail.com>
2016-10-22 19:59:35 -0400

    Add textile list component

---
+++ src/app/textile-list/textile-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { TextileListService } from './textile-list.service';
+
+@Component({
+  selector: 'app-textile-list',
+  providers: [TextileListService],
+  templateUrl: './textile-list.component.html',
+  styleUrls: ['./textile-list.component.css']
+})
+export class TextileListComponent implements OnInit {
+
+  textiles: Object[]
+  constructor(private texSvc:TextileListService) {
+    //texSvc.textiles.subscribe(
+    //  data => {
+    //    console.log(data);
+    //    this.textiles = data.json()
+    //  },
+    //  error => console.error('Error: ' + error)
+    //);
+  }
+
+  ngOnInit() {
+    console.log('on init...')
+    this.textiles = [
+      {"title": "test1", "imageUrl": "/angular/samples/circles.png"},
+      {"title": "test2", "imageUrl": "/angular/samples/hex.jpg"},
+      {"title": "test3", "imageUrl": "/angular/samples/triangle.png"}
+    ]
+    console.log(this.textiles)
+  }
+
+}
--- src/app/textile-list/textile-list.service.ts
+++ src/app/textile-list/textile-list.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable} from 'rxjs/Rx';
+//import { Observable} from 'rxjs/Observable';
+//import 'rxjs/add/operator/map';
+//import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class TextileListService {
+  textiles:Observable<Response>;
 
-  constructor() { }
+  constructor(private http:Http) {
+    //this.http = http
+    this.textiles = http.get("/src/assets/samples.json")
+      .catch(this.onError);
+  }
+  onError(res: any) {
+    console.log('on error has been called')
+    let msg = res.status + ":" + res.statusText
+    console.log(msg);
+    return Observable.throw(msg);
+  }
+  getTextiles() { //: {title:string, imageUrl:string}[] {
+    //return this.http.get("/app/textile/samples.json")
+    //  .map((res:Response) => res.json())
+    //  .catch((error:Any) =>  Observable.throw(error.json().error || 'Server error'))
+    //this.http.get("/app/textile/samples.json").then(response =>
+    //{
+    //    this.samples = response.json()
+    //},
+    //error => {
+    //    console.log("Error finding smaples.json " + error)
+    //})
+    //return this.samples
+
+    //return this.http.get("/app/textile/samples.json")
+
+    //return [
+    //  {"title": "test1", "imageUrl": "/angular/samples/circles.png"},
+    //  {"title": "test2", "imageUrl": "/angular/samples/hex.jpg"},
+    //  {"title": "test3", "imageUrl": "/angular/samples/triangle.png"}
+    //]
+  }
 
 }
