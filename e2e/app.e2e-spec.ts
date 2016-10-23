7088bc10ef512e171903fac3a814d53f9e97b179
Adam Boudreau <adamdboudreau@gmail.com>
2016-10-22 20:18:08 -0400

    New project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TextilesPage } from './app.po';
+
+describe('textiles App', function() {
+  let page: TextilesPage;
+
+  beforeEach(() => {
+    page = new TextilesPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
