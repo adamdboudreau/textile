df48c18c4a9b9cb9992d2be2188b7ad3f70c0639
Adam Boudreau <adamdboudreau@gmail.com>
2018-02-23 17:01:41 -0500

    Setup textile list component to display a few test images.

--- src/app/textile-list/textile-list.component.ts
+++ src/app/textile-list/textile-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TextileListService } from '../textile-list.service';
 
 @Component({
   selector: 'app-textile-list',
+  providers: [TextileListService],
   templateUrl: './textile-list.component.html',
   styleUrls: ['./textile-list.component.css']
 })
 export class TextileListComponent implements OnInit {
 
-  constructor() { }
+  textiles: Object[]
+  constructor(private texSvc:TextileListService) {
+    texSvc.textiles.subscribe(
+      data => {
+        console.log(data);
+        this.textiles = data.json()
+      },
+      error => console.error('Error: ' + error)
+    );
+  }
 
   ngOnInit() {
+    console.log('on init...')
   }
 
 }
